67d65fc3ff0924042490f08503ca4d69e446a296
Saumya999 <saumyadiptasarkar49@gmail.com>
2022-06-27 19:48:27 +0530

    Added scheduler for deleting and free up the numbers for OTP

--- src/main/java/in/crossdimension/notificationmanager/NotificationManagerApplication.java
+++ src/main/java/in/crossdimension/notificationmanager/NotificationManagerApplication.java
@@ -2,8 +2,11 @@ package in.crossdimension.notificationmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NotificationManagerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/in/crossdimension/notificationmanager/service/SchedulerService.java
@@ -0,0 +1,32 @@
+package in.crossdimension.notificationmanager.service;
+
+import in.crossdimension.notificationmanager.entity.SMS;
+import in.crossdimension.notificationmanager.repository.IMessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+public class SchedulerService {
+
+    @Autowired
+    private IMessageRepository messageRepository;
+
+    @Scheduled(cron = "0 */60 * ? * *")
+    public void runHourlyJobForOTP() {
+        String currentTime = getTimeStamp();
+        List<SMS> smsList = messageRepository.findByTimeStamp(currentTime);
+        System.out.println(smsList);
+        messageRepository.deleteAll(smsList);
+    }
+
+    private String getTimeStamp() {
+        LocalDateTime currentTime = LocalDateTime.now();
+        currentTime = currentTime.minusMinutes(2);
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(currentTime);
+    }
+}
